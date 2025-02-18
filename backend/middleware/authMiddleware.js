78e7f7f767ceae3094490cb55b2af588902d3a92
dev-SidD <sid25jun03@gmail.com>
2025-02-19 02:58:11 +0530

    add commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,16 +4,16 @@ const authMiddleware = (req, res, next) => {
   try {
     // Check if the Authorization header contains the token
     const token = req.headers.authorization?.split(" ")[1];
-    console.log(token)
+    
     if (!token) {
-      console.log("hat")
+      
       return res.status(401).json({ msg: "No token, authorization denied" });
     }
     
     // Verify the token using the JWT_SECRET
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // Attach decoded user data (e.g., id, username) to req.user
-    console.log(req.user)
+    
     next(); // Proceed to the next middleware or route handler
   } catch (error) {
     console.error("Authentication error:", error);
